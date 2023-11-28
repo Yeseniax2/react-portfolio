f1951e2d4272ef84a4ff9fe0308e46a3559f60e5
yesenia <yeseniab743@gmail.com>
2023-11-27 20:22:01 -0500

    imported react

---
+++ componets/home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+
+function Home() {
+  return (
+    <section>
+      <Container fluid className="home-section" id="home">
+        <Particle />
+        <Container className="home-content">
+          <Row>
