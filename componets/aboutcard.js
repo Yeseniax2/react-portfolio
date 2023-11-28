1fcfc657ab0c9a5f00fc189f36490efbdd67260e
yesenia <yeseniab743@gmail.com>
2023-11-28 13:35:56 -0500

    added an about card creqting an intro

---
+++ componets/aboutcard.js
@@ -0,0 +1,14 @@
+function Aboutcard () {
+    rreturn (
+        <card class className="quote-card-view">
+            <card.body>
+                <blockquote className="blockquote mb-0">
+                    <p style= {{textAlign: "justify"}}></p>
+                    Hello welcome, My Name <span className="pink">Yesenia Benitez</span>
+                </blockquote>
+        </card.body>
+        
+        </card>
+    )}
+
+<footer className="blockquote-footer">Yesenia Benitez</footer>
