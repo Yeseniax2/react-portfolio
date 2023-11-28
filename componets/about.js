7445d5d32335c6c5b9abaf8c0af489d959f31f39
yesenia <yeseniab743@gmail.com>
2023-11-28 13:18:57 -0500

    added about to page

---
+++ componets/about.js
@@ -0,0 +1,28 @@
+
+
+
+
+function About() {
+    return (
+      <Container fluid className="about-section">
+      <Particle />
+      <Container>
+      <Row style={{ justifyContent: "center", padding: "10px" }}>
+      <Col
+         md={7}
+        style={{
+                justifyContent: "center",
+                paddingTop: "30px",
+                paddingBottom: "50px",
+              }}
+              >
+        <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+                 <strong className="pink">I'M</strong>
+              </h1>
+              <Aboutcard />
+            </Col>
+            <Col
+            md={5}
+            style={{ paddingTop: "120px", paddingBottom: "50px" }}
+            className="about-img"
+          ></Col>
